999527a60a3ccb68cf53c1b1da20b22b19c0a9ba
Sayed Rahman <s.rahman14@hotmail.com>
2024-11-23 00:17:10 +0000

    updated sequence problem

--- problemSolving/mySequence.py
+++ problemSolving/mySequence.py
@@ -1,36 +1,14 @@
-# Starting numbers - can be 0, 1
+# Starting numbers - can be 17, 25
 num1 = 17
 num2 = 25
 
 # Growth rules
-# Rule 1: Add the number of digits in the second-to-last number to the most recent number.
-# Rule 2: If the last number has an odd number of digits, subtract the sum of its digits from the second-to-last number.
-# Rule 3: If both the last two numbers end in the same digit, multiply their sum by 3.
-
-
-def add_second_to_last(num1, num2):
-    # Convert second to last number into a string so I can count the digits
-    # Add the number of digits to the last number
-    num1_str = str(num1)
-    return num2 + len(num1_str)
-
-
-def check_if_odd(num1, num2):
-    # If so, add the sum of the digits to the second to last number
-    # Create a list for each digit in the number
-    # Convert the number into a string and then back to an interger
-    num2_list_digits = [int(digit) for digit in str(num2)]
-    # If the number of digits are odd, then return sum of the second-to-last number with the
-    # sum of the digits
-
-    return num1 - sum(num2_list_digits)
-
-
-def check_last_digit(num1, num2):
-    # Use index splicing to access the last digit for both numbers, after converting the numbers
-    # to strings to check if they are the same, if so then add the sum and multiply the sum by
-    # three
-    return (num1 + num2) * 3
+# Rule 1: If both numbers end with the same digit,
+# from the second-to-last number.
+# multiply the number of digits in the second-to-last number to the most recent number.
+# Rule 2: If the last number has an odd number of digits, add the square of the sum of its digits
+# from the second-to-last number.
+# Rule 3: If none of these conditions are met, then multiply the sum of the two numbers by 2
 
 
 sequence = []
@@ -40,27 +18,33 @@ sequence.append(num2)
 # Using a while loop to generate first N number of the sequence:
 
 count = 0
-n = 6
+n = 8
 # Minus by 1 to control index, as index begins from 1, this will always be the most recent number
 
 while count < n:
     # Important to include this inside the while loop so it continues to update as the sequence
     # grows
     length_sequence = len(sequence) - 1
+    # If the last digits of both two numbers are the same
     if str(sequence[length_sequence - 1])[-1:] == str(sequence[length_sequence])[-1:]:
-        result = check_last_digit(
-            sequence[length_sequence], sequence[length_sequence - 1])
+
+        num1_str = str(sequence[length_sequence])
+        result = sequence[length_sequence - 1] * len(num1_str)
         sequence.append(result)
         count += 1
     # Check if the most recent number has odd number of digits
+    # if the length of the digits is not even of the newest number is not even
     elif len(str(sequence[length_sequence])) % 2 != 0:
-        result = check_if_odd(
-            sequence[length_sequence - 1], sequence[length_sequence])
+        num2_list_digits = [int(digit)
+                            for digit in str(sequence[length_sequence - 1])]
+        result = sequence[length_sequence] + (sum(num2_list_digits) ** 2)
         sequence.append(result)
         count += 1
+    # If none of the previous 2 rules are met
     else:
-        result = add_second_to_last(
-            sequence[length_sequence - 1], sequence[length_sequence])
+        num1 = sequence[length_sequence - 1]
+        num2 = sequence[length_sequence]
+        result = (num1 + num2) * 2
         sequence.append(result)
         count += 1
 
